dc374494b6d6ed0d6b694db6958c96b18b66eb1a
rarufino27-alt <rarufino27@gmail.com>
2026-01-14 21:57:38 -0300

    Criar financeiro.js

---
+++ financeiro.js
@@ -0,0 +1,38 @@
+const Financeiro = {
+
+  custoFixoMensal() {
+    const fixos = DataManager.get("custos.fixos") || [];
+    return fixos.reduce((t, c) => t + Number(c.valor), 0);
+  },
+
+  custoVariavelProduto(produto) {
+    return (
+      Number(produto.custoCompra || 0) +
+      Number(produto.frete || 0) +
+      Number(produto.impostosCompra || 0) +
+      Number(produto.perdas || 0)
+    );
+  },
+
+  custoTotalProduto(produto) {
+    return this.custoVariavelProduto(produto);
+  },
+
+  precoVenda(produto, margemPercentual) {
+    const custo = this.custoTotalProduto(produto);
+    const margem = margemPercentual / 100;
+    return custo / (1 - margem);
+  },
+
+  lucroPorProduto(produto, precoVenda) {
+    return precoVenda - this.custoTotalProduto(produto);
+  },
+
+  pontoEquilibrio() {
+    const fixo = this.custoFixoMensal();
+    const vendas = DataManager.get("vendas") || [];
+
+    const margemTotal = vendas.reduce((t, v) => t + v.lucro, 0);
+    return margemTotal > 0 ? fixo / margemTotal : 0;
+  }
+};
