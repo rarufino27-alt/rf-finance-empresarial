28d0a27b9eb891abb9f50cdfe493df5d093d7330
rarufino27-alt <rarufino27@gmail.com>
2026-01-21 07:13:11 -0300

    Atualizar o datamanager.js

--- core/datamanager.js
+++ core/datamanager.js
@@ -1,36 +1,34 @@
 const DataManager = {
   data: {
-    empresa: {},
-    custos: {
-      fixos: [],
-      variaveis: []
+    empresa: {
+      regime: null, // mei | simples
+      tipo: null    // comercio | industria
     },
+    funcionarios: [],
     produtos: [],
     vendas: []
   },
 
   init() {
-    const saved = Storage.get("rf_finance_data");
-    if (saved) this.data = saved;
+    const saved = localStorage.getItem("rf_finance");
+    if (saved) this.data = JSON.parse(saved);
   },
 
   save() {
-    Storage.set("rf_finance_data", this.data);
+    localStorage.setItem("rf_finance", JSON.stringify(this.data));
   },
 
   get(path) {
-    return path.split(".").reduce((obj, key) => obj?.[key], this.data);
+    return path.split(".").reduce((o, k) => o?.[k], this.data);
   },
 
   set(path, value) {
     const keys = path.split(".");
     let obj = this.data;
-
     keys.slice(0, -1).forEach(k => {
       if (!obj[k]) obj[k] = {};
       obj = obj[k];
     });
-
     obj[keys[keys.length - 1]] = value;
     this.save();
   },
